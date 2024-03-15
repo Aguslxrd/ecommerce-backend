dee21864b54c5e2d666bed55c8ebad04db8012f7
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-03-15 18:08:00 -0300

    category entity

--- src/main/java/com/damian/ecommerce/backend/infrastructure/Entity/CategoryEntity.java
+++ src/main/java/com/damian/ecommerce/backend/infrastructure/Entity/CategoryEntity.java
@@ -1,4 +1,20 @@
 package com.damian.ecommerce.backend.infrastructure.Entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
 public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private LocalDateTime dateCreatedAt;
+    private LocalDateTime dateUpdatedAt;
 }
