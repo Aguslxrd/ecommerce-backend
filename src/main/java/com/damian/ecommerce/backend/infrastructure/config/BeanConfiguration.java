21706b0eb90d72385dab5aed0cde2157459027fb
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-03-15 18:41:56 -0300

    category bean

--- src/main/java/com/damian/ecommerce/backend/infrastructure/config/BeanConfiguration.java
+++ src/main/java/com/damian/ecommerce/backend/infrastructure/config/BeanConfiguration.java
@@ -1,6 +1,8 @@
 package com.damian.ecommerce.backend.infrastructure.config;
 
+import com.damian.ecommerce.backend.application.CategoryService;
 import com.damian.ecommerce.backend.application.UserService;
+import com.damian.ecommerce.backend.domain.port.ICategoryRepository;
 import com.damian.ecommerce.backend.domain.port.IUserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +12,10 @@ public class BeanConfiguration {
     @Bean
     public UserService userService(IUserRepository iUserRepository){
         return new UserService(iUserRepository);
+    }
+
+    @Bean
+    public CategoryService categoryService(ICategoryRepository iCategoryRepository){
+        return new CategoryService(iCategoryRepository);
     }
 }
