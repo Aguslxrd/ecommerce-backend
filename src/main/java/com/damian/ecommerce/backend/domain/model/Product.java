24acb7b141a98eea45d5593fca4908cd22d699f7
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-03-15 14:50:36 -0300

    products model

---
+++ src/main/java/com/damian/ecommerce/backend/domain/model/Product.java
@@ -0,0 +1,21 @@
+package com.damian.ecommerce.backend.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class Product {
+    private Integer id;
+    private String name;
+    private String code;
+    private String description;
+    private String urlImage;
+    private BigDecimal price;
+    private LocalDateTime dateCreatedAt;
+    private LocalDateTime dateUpdatedAt;
+    private Integer userId;
+}
--- src/main/java/com/damian/ecommerce/backend/domain/model/User.java
+++ src/main/java/com/damian/ecommerce/backend/domain/model/User.java
@@ -1,7 +1,12 @@
 package com.damian.ecommerce.backend.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
 public class User {
 
     private Integer id;
