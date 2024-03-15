604b46cac8427a669495c7c171564d81bbc48f06
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-03-15 20:41:25 -0300

    productcrudrepository implement

--- src/main/java/com/damian/ecommerce/backend/infrastructure/Adapter/ProductCrudRepositoryImpl.java
+++ src/main/java/com/damian/ecommerce/backend/infrastructure/Adapter/ProductCrudRepositoryImpl.java
@@ -2,25 +2,41 @@ package com.damian.ecommerce.backend.infrastructure.Adapter;
 
 import com.damian.ecommerce.backend.domain.model.Product;
 import com.damian.ecommerce.backend.domain.port.IProductRepository;
+import com.damian.ecommerce.backend.infrastructure.Mapper.ProductMapper;
 
 public class ProductCrudRepositoryImpl implements IProductRepository {
+
+    private final IProductCrudRepository iProductCrudRepository;
+
+    private final ProductMapper productMapper;
+
+    public ProductCrudRepositoryImpl(IProductCrudRepository iProductCrudRepository, ProductMapper productMapper) {
+        this.iProductCrudRepository = iProductCrudRepository;
+        this.productMapper = productMapper;
+    }
+
     @Override
     public Product save(Product product) {
-        return null;
+        return productMapper.toProduct(iProductCrudRepository.save(productMapper.toProductEntity(product)));
     }
 
     @Override
     public Iterable<Product> findAll() {
-        return null;
+        return productMapper.toProductList(iProductCrudRepository.findAll());
     }
 
     @Override
     public Product findById(Integer id) {
-        return null;
+        return productMapper.toProduct(iProductCrudRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Producto con "+id+" no existe")
+        ));
     }
 
     @Override
     public void deleteById(Integer id) {
-
+        iProductCrudRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Producto con id " +id+" no existe")
+        );
+        iProductCrudRepository.deleteById(id);
     }
 }
