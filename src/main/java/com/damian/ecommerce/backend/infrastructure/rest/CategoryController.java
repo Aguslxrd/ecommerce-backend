3af46bff0371d85d7058c0f53e24d08453786bbd
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-03-15 19:54:39 -0300

    response entity en categories controller

--- src/main/java/com/damian/ecommerce/backend/infrastructure/rest/CategoryController.java
+++ src/main/java/com/damian/ecommerce/backend/infrastructure/rest/CategoryController.java
@@ -4,6 +4,8 @@ import com.damian.ecommerce.backend.application.CategoryService;
 import com.damian.ecommerce.backend.domain.model.Category;
 import com.damian.ecommerce.backend.domain.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,25 +15,32 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     public CategoryController(CategoryService categoryService) {
+
         this.categoryService = categoryService;
     }
 
     @PostMapping
-    public Category save(@RequestBody Category category){
-        return categoryService.save(category);
+    public ResponseEntity<Category> save(@RequestBody Category category){
+        if (category == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(categoryService.save(category), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Category findById(@PathVariable Integer id){
-        return  categoryService.findById(id);
+    public ResponseEntity<Category> findById(@PathVariable Integer id){
+        return ResponseEntity.ok(categoryService.findById(id));
     }
 
     @GetMapping
-    public Iterable<Category> findAll(){return categoryService.findAll();}
+    public ResponseEntity<Iterable<Category>> findAll(){
+        return ResponseEntity.ok(categoryService.findAll());
+    }
 
     @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable Integer id){
+    public ResponseEntity<HttpStatus> deleteById(@PathVariable Integer id){
         categoryService.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 
 }
