ad9dd895870a3bbeef61300cb31eae0949177664
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-03-15 20:31:49 -0300

    product mapper

---
+++ src/main/java/com/damian/ecommerce/backend/infrastructure/Mapper/ProductMapper.java
@@ -0,0 +1,30 @@
+package com.damian.ecommerce.backend.infrastructure.Mapper;
+
+import com.damian.ecommerce.backend.domain.model.Category;
+import com.damian.ecommerce.backend.domain.model.Product;
+import com.damian.ecommerce.backend.infrastructure.Entity.CategoryEntity;
+import com.damian.ecommerce.backend.infrastructure.Entity.ProductEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    @Mappings({
+            @Mapping(source = "id", target = "id"),
+            @Mapping(source = "name", target = "name"),
+            @Mapping(source = "code", target = "code"),
+            @Mapping(source = "description", target = "description"),
+            @Mapping(source = "urlImage", target = "urlImage"),
+            @Mapping(source = "price", target = "price"),
+            @Mapping(source = "dateCreatedAt", target = "dateCreatedAt"),
+            @Mapping(source = "dateUpdatedAt", target = "dateUpdatedAt"),
+            @Mapping(source = "user.id", target = "userId"),
+            @Mapping(source = "categoryEntity.id", target = "categoryId")
+    })
+    Product toProduct(ProductEntity productEntity);
+
+    Iterable<Product> toProductList(Iterable<ProductEntity> productEntities);
+
+    ProductEntity toProductEntity(Product product);
+}
