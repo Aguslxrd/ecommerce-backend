65343c3b7c5f8e7c274a6eb60fdcd8df081d2773
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-03-15 17:09:39 -0300

    mappings

--- src/main/java/com/damian/ecommerce/backend/infrastructure/Mapper/UserMapper.java
+++ src/main/java/com/damian/ecommerce/backend/infrastructure/Mapper/UserMapper.java
@@ -1,7 +1,31 @@
 package com.damian.ecommerce.backend.infrastructure.Mapper;
 
+import com.damian.ecommerce.backend.domain.model.User;
+import com.damian.ecommerce.backend.infrastructure.UserEntity;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface UserMapper {
+
+    @Mappings(
+            {
+                    @Mapping(source = "id", target = "id"),
+                    @Mapping(source = "username", target = "username"),
+                    @Mapping(source = "firstName", target = "firstName"),
+                    @Mapping(source = "email", target = "email"),
+                    @Mapping(source = "address", target = "address"),
+                    @Mapping(source = "cellphone", target = "cellphone"),
+                    @Mapping(source = "password", target = "password"),
+                    @Mapping(source = "userType", target = "userType"),
+                    @Mapping(source = "dateCreatedAt", target = "dateCreatedAt"),
+                    @Mapping(source = "dateUpdated", target = "dateUpdatedAt")
+            }
+    )
+    User toUser(UserEntity userEntity);
+    Iterable<User> toUsers(Iterable<UserEntity> userEntities);
+    @InheritInverseConfiguration
+    UserEntity toUserEntity(User user);
 }
