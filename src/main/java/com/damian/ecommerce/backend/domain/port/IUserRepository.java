849e3190e396b6c7962e8ba9fe31a17df6cb8601
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-03-15 15:31:53 -0300

    iuser repository

---
+++ src/main/java/com/damian/ecommerce/backend/domain/port/IUserRepository.java
@@ -0,0 +1,9 @@
+package com.damian.ecommerce.backend.domain.port;
+
+import com.damian.ecommerce.backend.domain.model.User;
+
+public interface IUserRepository {
+
+    User save(User user);
+    User findByEmail(String email);
+}
