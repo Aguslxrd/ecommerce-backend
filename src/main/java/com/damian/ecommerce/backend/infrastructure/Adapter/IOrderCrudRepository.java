733771878d819e16b5ee14d02da782373acc0048
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-03-16 16:32:33 -0300

    bean de servicio y bug fix de query con error

--- src/main/java/com/damian/ecommerce/backend/infrastructure/Adapter/IOrderCrudRepository.java
+++ src/main/java/com/damian/ecommerce/backend/infrastructure/Adapter/IOrderCrudRepository.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 public interface IOrderCrudRepository extends CrudRepository<OrderEntity, Integer> {
     @Transactional
     @Modifying
-    @Query("UPDATE OrderEntity o SET o.state = :state WHERE o.id = :orderId")
+    @Query("UPDATE OrderEntity o SET o.orderState = :state WHERE o.id = :orderId")
     void updateStateById(Integer orderId, OrderState state);
 
     Iterable<OrderEntity> findByUserEntity(UserEntity userEntity);
--- src/main/java/com/damian/ecommerce/backend/infrastructure/config/BeanConfiguration.java
+++ src/main/java/com/damian/ecommerce/backend/infrastructure/config/BeanConfiguration.java
@@ -1,9 +1,11 @@
 package com.damian.ecommerce.backend.infrastructure.config;
 
 import com.damian.ecommerce.backend.application.CategoryService;
+import com.damian.ecommerce.backend.application.OrderService;
 import com.damian.ecommerce.backend.application.ProductService;
 import com.damian.ecommerce.backend.application.UserService;
 import com.damian.ecommerce.backend.domain.port.ICategoryRepository;
+import com.damian.ecommerce.backend.domain.port.IOrderRepository;
 import com.damian.ecommerce.backend.domain.port.IProductRepository;
 import com.damian.ecommerce.backend.domain.port.IUserRepository;
 import org.springframework.context.annotation.Bean;
@@ -25,5 +27,10 @@ public class BeanConfiguration {
     @Bean
     public ProductService productService(IProductRepository iProductRepository){
         return new ProductService(iProductRepository);
+    }
+
+    @Bean
+    public OrderService orderService(IOrderRepository iOrderRepository){
+        return new OrderService(iOrderRepository);
     }
 }
