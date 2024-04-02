b2afe1a09f3ee3cabc698d7db10ea58a39caf123
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-04-02 17:02:11 -0300

    validacion de token jwt

---
+++ src/main/java/com/damian/ecommerce/backend/infrastructure/jwt/JwtValidation.java
@@ -0,0 +1,36 @@
+package com.damian.ecommerce.backend.infrastructure.jwt;
+
+import com.damian.ecommerce.backend.infrastructure.Service.CustomUserDetailService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import static com.damian.ecommerce.backend.infrastructure.jwt.Constants.*;
+
+public class JwtValidation {
+    public static boolean tokenExistsOnRequest(HttpServletRequest request, HttpServletResponse){
+        String header = request.getHeader(HEADER_AUTHORIZATION);
+        if (header == null || !header.startsWith(TOKEN_BEARER_PREFIX)){
+            return false;
+        }
+        return true;
+    }
+
+    public static Claims JWTIsValid(HttpServletRequest request){
+        String jwtToken = request.getHeader(HEADER_AUTHORIZATION).replace(TOKEN_BEARER_PREFIX, "");
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignedKey(SECRET_KEY))
+                .build().parseClaimsJwt(jwtToken).getBody();
+    }
+
+    //autentica al usuario credenciales y role
+    public static void setAuthentication(Claims claims, CustomUserDetailService customUserDetailService){
+        UserDetails userDetails = customUserDetailService.loadUserByUsername(claims.getSubject()); // trae el username = email de la persona autenticada
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+}
