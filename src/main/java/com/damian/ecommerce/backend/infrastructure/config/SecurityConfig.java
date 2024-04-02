44a957f13d4be45ad49105330a44b4d6ad5bfacb
Damian <78635584+Aguslxrd@users.noreply.github.com>
2024-04-02 14:11:02 -0300

    contraseñas encriptadas al momento de registrarse

--- src/main/java/com/damian/ecommerce/backend/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/damian/ecommerce/backend/infrastructure/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -13,9 +14,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf( csrf-> csrf.disable()).authorizeHttpRequests(
-                auth -> auth.requestMatchers("api/v1/auth").permitAll().anyRequest().authenticated()
+                auth -> auth.requestMatchers("api/v1/auth/**").permitAll().anyRequest().authenticated()
         );
         return  httpSecurity.build();
     }
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/damian/ecommerce/backend/infrastructure/rest/RegisterController.java
+++ src/main/java/com/damian/ecommerce/backend/infrastructure/rest/RegisterController.java
@@ -2,18 +2,23 @@ package com.damian.ecommerce.backend.infrastructure.rest;
 
 import com.damian.ecommerce.backend.application.RegistrationService;
 import com.damian.ecommerce.backend.domain.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/auth")
 @CrossOrigin(origins = "http://localhost:4200")
+@Slf4j
 public class RegisterController {
     private final RegistrationService registrationService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public RegisterController(RegistrationService registrationService) {
+    public RegisterController(RegistrationService registrationService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.registrationService = registrationService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @PostMapping("/register")
@@ -22,6 +27,7 @@ public class RegisterController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         return new ResponseEntity<>(registrationService.register(user), HttpStatus.CREATED);
     }
 
